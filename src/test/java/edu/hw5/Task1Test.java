f0edab4b78dfbcf4faabd4e4db4215f8b14e33b2
Pacan4ik <andreyka.potekhin@mail.ru>
2023-11-09 16:36:15 +0300

    task 1(2)

--- src/test/java/edu/hw5/Task1Test.java
+++ src/test/java/edu/hw5/Task1Test.java
@@ -1,2 +1,10 @@
-package edu.hw5;public class Task1Test {
+package edu.hw5;
+
+import java.time.*;
+import java.util.regex.Pattern;
+
+public class Task1Test {
+    public static void main(String[] args) {
+        System.out.println(Task1.getComputerClubStats(new String[]{"2022-03-12, 20:20 - 2022-03-12, 23:50"}));
+    }
 }
--- src/test/java/edu/hw5/Task2Test.java
+++ src/test/java/edu/hw5/Task2Test.java
@@ -1,2 +1,14 @@
-package edu.hw5;public class Task2Test {
+package edu.hw5;
+
+import java.time.LocalDate;
+
+public class Task2Test {
+    public static void main(String[] args) {
+        System.out.println(Task2.getBadFridays(1925));
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+        System.out.println();
+        System.out.println(localDate.);
+
+    }
 }
